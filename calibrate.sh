6d2db627d6cdf5270aac632abce7126bb636a9c2
kazumakano <kazumakano@outlook.jp>
2025-05-01 19:53:56 +0900

    add calibrate.sh

---
+++ calibrate.sh
@@ -0,0 +1,13 @@
+# Conduct calibration with bag.
+
+BAG_DIR=$1                   # Path to bag directory.
+BOARD_FILE=$2                # Path to board file.
+CAM_NAME=$3                  # Camera name.
+RESULT_DIR=$4                # Path to result directory.
+FILE_OR_STREAM=${5:-file}    # Whether file or stream. Default is file.
+
+if [ $FILE_OR_STREAM = file ]; then
+    rosrun kalibr tartan_calibrate --bag $BAG_DIR/$CAM_NAME.bag --target $BOARD_FILE --topics /camera/image_raw --models ds-none --save_dir $RESULT_DIR/$CAM_NAME/
+elif [ $FILE_OR_STREAM = stream ]; then
+    rosrun kalibr tartan_calibrate --bag $BAG_DIR/$CAM_NAME.bag --target $BOARD_FILE --topics /cam/image_raw --models ds-none --save_dir $RESULT_DIR/$CAM_NAME/
+fi
