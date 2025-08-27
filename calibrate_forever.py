8c05069a4ec749eef4aa795bb05ce4f2ac57126a
kazumakano <kazumakano@outlook.jp>
2025-08-27 06:19:26 +0000

    fix calibrate_forever.py

--- calibrate_forever.py
+++ calibrate_forever.py
@@ -5,25 +5,28 @@ from asyncio import subprocess as async_subprocess
 
 MAX_TASK_NUM  = 10
 
+def _on_task_done(done_task: asyncio.Task, result_dir: str, task_queue: dict[str, dict[str, async_subprocess.Process | asyncio.Task]]) -> None:
+    for cn, d in task_queue.items():
+        if d["task"] == done_task:
+            break
+    print(f"calibration {'completed' if path.exists(path.join(result_dir, cn, 'log1-camchain.yaml')) else 'failed'} for camera {cn}")
+    task_queue.pop(cn)
+
 async def cali(board_file: str, result_dir: str, use_stream: bool = False) -> None:
     task_queue: dict[str, dict[str, async_subprocess.Process | asyncio.Task]] = {}
 
     try:
         while True:
-            for cn_1 in os.listdir(result_dir):
-                if path.exists(path.join(result_dir, cn_1, "video.bag")) and not path.exists(path.join(result_dir, cn_1, "log1-camchain.yaml")):
+            for cn in os.listdir(result_dir):
+                if path.exists(path.join(result_dir, cn, "video.bag")) and not path.exists(path.join(result_dir, cn, "log1-camchain.yaml")) and cn not in task_queue.keys():
                     if len(task_queue) >= MAX_TASK_NUM:
-                        done_tasks, _ = await asyncio.wait([d["task"] for d in task_queue.values()], return_when=asyncio.FIRST_COMPLETED)
-                        for cn_2, d in task_queue.copy().items():
-                            if d["task"] in done_tasks:
-                                print(f"calibration {'completed' if path.exists(path.join(result_dir, cn_2, 'log1-camchain.yaml')) else 'failed'} for camera {cn_2}")
-                                task_queue.pop(cn_2)
-                                break
+                        await asyncio.wait([d["task"] for d in task_queue.values()], return_when=asyncio.FIRST_COMPLETED)
 
-                    proc = await asyncio.create_subprocess_shell(f"/app/calibrate.sh {board_file} {cn_1} {result_dir} {'stream' if use_stream else 'file'} > {path.join(result_dir, cn_1, 'log.txt')} 2> {path.join(result_dir, cn_1, 'error.txt')}")
-                    task_queue[cn_1] = {"proc": proc, "task": asyncio.create_task(proc.wait())}
+                    proc = await asyncio.create_subprocess_shell(f"/app/calibrate.sh {board_file} {cn} {result_dir} {'stream' if use_stream else 'file'} > {path.join(result_dir, cn, 'log.txt')} 2> {path.join(result_dir, cn, 'error.txt')}")
+                    task_queue[cn] = {"proc": proc, "task": asyncio.create_task(proc.wait())}
+                    task_queue[cn]["task"].add_done_callback(lambda t: _on_task_done(t, result_dir, task_queue))
 
-                await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
     except:
         for d in task_queue.values():
